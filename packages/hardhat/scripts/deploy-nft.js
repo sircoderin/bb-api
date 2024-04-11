58cf54ec997c694064791f73c8f0f6cc7c3bd1b8
andrewtookay <andrewtookay@gmail.com>
2024-04-11 15:45:53 +0300

    add user rating and signature checks on update mintable NFTs

--- packages/hardhat/scripts/deploy-nft.js
+++ packages/hardhat/scripts/deploy-nft.js
@@ -1,17 +1,17 @@
 async function main() {
-    // Grab the contract factory 
-    const MyNFT = await ethers.getContractFactory("BubbleBreakerBadge");
- 
-    // Start deployment, returning a promise that resolves to a contract object
-    const myNFT = await MyNFT.deploy(); // Instance of the contract 
+  // Grab the contract factory 
+  const bbbNft = await ethers.getContractFactory("BubbleBreakerBadge");
 
-    console.log(myNFT);
-    console.log("Contract deployed to address:", myNFT.target);
+  // Start deployment, returning a promise that resolves to a contract object
+  const deployedBbbNft = await bbbNft.deploy(); // Instance of the contract 
+
+  console.log(deployedBbbNft);
+  console.log("Contract deployed to address:", deployedBbbNft.target);
  }
  
- main()
-   .then(() => process.exit(0))
-   .catch(error => {
-     console.error(error);
-     process.exit(1);
-   });
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
